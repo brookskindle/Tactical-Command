aa291398b0149f095c265e19d9f3ace54bb0ee14
Brooks <brooks.kindle@wsu.edu>
2014-01-03 22:04:40 -0800

    working on creating the sprites for game screen (in playTurn method)

--- GameScreen.cpp
+++ GameScreen.cpp
@@ -30,14 +30,77 @@ bool GameScreen::loaded() const {
 /* Displays the game screen to the window and plays the hero's turn.
 This means, the hero is allowed to decide which of the other players he wants
 to attack. and then does so */
-void GameScreen::playTurn(sf::RenderWindow &window, const Player &hero,
-                          const vector<Player *> &others) const {
+GameScreen::MenuAction GameScreen::playTurn(sf::RenderWindow &window,
+                const Player &hero, const vector<Player *> &others) const {
+    MenuAction action = Failure;
+    if(!loaded()) {
+        return action;
+    }
+    action = Continue;
+
+    //Step 0: create the sprites for our game screen
+    struct TokenButton {
+        int playerId; //id of the player the token belongs to
+        sf::Sprite sprite; //drawable sprite of the token
+        Coordinate coord; //coordinate of the token on the player's board
+    };
+    vector<sf::Sprite> heroSprites; //token sprites for our hero
+    vector<TokenButton> otherSprites; //token buttons for the other players
+
+    //create the sprites for our hero first
+    sf::Vector2f position;
+    position.x = 341.0f;
+    position.y = 300.0f;
+    for(int i = 0; i < hero.board().rows(); i++) {
+        sf::Sprite sprite;
+        for(int j = 0; j < hero.board().columns(); j++) {
+            Coordinate c(i, j); //token coordinate
+            sprite = spriteOf(hero.board()[c]);
+            sprite.setPosition(position);
+            position.x += sprite.getTextureRect().width;
+            heroSprites.push_back(sprite);
+        }//end for j
+        position.y += sprite.getTextureRect().height;
+        position.x = 341.0f;
+    }//end for i
+
+    //create the sprites for the others
+    int nPlayers = 0; //number of players we've created sprites for
+    for(Player *p : others) {
+        if(!p) { continue; }
+
+        sf::Vector2f position;
+        position.x = 341.0f;
+        position.y = 300.0f;
+        for(int i = 0; i < hero.board().rows(); i++) {
+            sf::Sprite sprite;
+            for(int j = 0; j < hero.board().columns(); j++) {
+                Coordinate c(i, j); //token coordinate
+                sprite = spriteOf(hero.board()[c]);
+                sprite.setPosition(position);
+                position.x += sprite.getTextureRect().width;
+                heroSprites.push_back(sprite);
+            }//end for j
+            position.y += sprite.getTextureRect().height;
+            position.x = 341.0f;
+        }//end for i
+
+        nPlayers++;
+        if(nPlayers >= 3) { 
+            break; //done creating sprites for our players
+        }
+    }//end for Player *p
+
     //TODO: finish this function
+    //Step 1: draw the sprites to the window
+    //Step 2: handle window events that transpire
+
+    return action;
 }//end playTurn
 
 
 /* Returns a sprite of the given ship token */
-sf::Sprite GameScreen::spriteOf(Token ship) {
+sf::Sprite GameScreen::spriteOf(Token ship) const {
     if(!loaded()) { return sf::Sprite(); }
 
     sf::Sprite sprite;
--- GameScreen.h
+++ GameScreen.h
@@ -15,13 +15,16 @@ class Player;
 
 class GameScreen {
     public:
+        enum MenuAction {
+            Failure, Close, Continue
+        };
         GameScreen();
         ~GameScreen();
         bool loaded() const;
-        void playTurn(sf::RenderWindow &window, const Player &hero,
-                  const std::vector<Player *> &others) const;
+        MenuAction playTurn(sf::RenderWindow &window, const Player &hero,
+                            const std::vector<Player *> &others) const;
     private:
-        sf::Sprite spriteOf(Token ship);
+        sf::Sprite spriteOf(Token ship) const;
         bool _loaded;
         sf::Texture _ships;
         sf::Texture _background;
